a731d823ea8deaeb57561cc8b4c998724dc13ba4
kouete kenneth <kouete678@gmail.com>
2024-08-17 08:20:19 +0100

    setting screen developmentvstarted

--- Source_Code/findit/components/menu.jsx
+++ Source_Code/findit/components/menu.jsx
@@ -75,7 +75,7 @@ const Menu = () => {
                   </Link>
                 </TouchableOpacity>
                 <TouchableOpacity className="mb-5 flex-row justify-center border-b-2 border-b-slate-200 pb-2">
-                  <Link href="/notification">
+                  <Link href="/settings">
                     <FontAwesome name="cog" size={20} className='text-center font-Roboto text-xl'/>
                     <Text className='text-center font-Roboto text-xl'> Settings</Text>
                   </Link>
